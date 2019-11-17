c18b440a119dfbae77b1a5a8641f1a0815e1ea94
JMEChristian <jmechristian@gmail.com>
2019-11-16 21:32:29 -0500

    closer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 export default () => (
-  <div>
-    <div>
-      <h1 className='headline-text text-primary'>Next.js + Tailwind css</h1>
+  <div className='flex w-full h-screen z-40 absolute'>
+    <div className='lg:w-1/2 bg-lowlight'>Left</div>
+    <div className='lg:w-1/2 bg-white flex'>
+      <div className='w-full flex justify-center items-center'>
+        <div className='w-1/2'>
+          <div className='font-extrabold headline-text mb-4'>
+            Tell Your Company Story.
+          </div>
+          <div className='text-2xl'>
+            We build process-driven digital solutions to help maximize your ROI.
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
